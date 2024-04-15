c3b209f7e418b283ee019f7153c61da8c143f24d
Misael <Misael@DESKTOP-D4QTLK9>
2024-04-14 20:45:57 -0400

    Agregar archivos de proyecto.

---
+++ Reto1Sintaxis/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+// Web oficial de C#:  https://docs.microsoft.com/es-es/dotnet/csharp/
+
+//Comentario de una línea
+
+/*Comentario 
+ * multilinea*/
+
+var primerVariable = "Primer variable";
+const string primerConstante = "Mi primer constante";
+
+int miInt;
+long miLong;
+float miFloat;
+double miDouble;
+decimal miDecimal;
+string miString;
+char miChar;
+bool miBoolean;
+
+Console.WriteLine("Hola, C#");
+
